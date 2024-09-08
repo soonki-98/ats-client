7a38bfbe42b017ac132ad9ebd03873b3fc5ee864
Min Soonki <minsoonki@ip-192-168-250-26.ap-northeast-2.compute.internal>
2024-09-08 18:04:46 +0900

    gnb 로고 클릭시 홈으로 이동

--- src/widgets/gnb/ui/index.tsx
+++ src/widgets/gnb/ui/index.tsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
 import { StyledGnbWrapper } from './styles';
 
 export default function Gnb() {
   return (
     <StyledGnbWrapper>
-      <div>로고</div>
-      <div>유저 프로필</div>
+      <ul>
+        <li>
+          <Link to="/">로고</Link>
+        </li>
+        <li>유저 프로필</li>
+      </ul>
     </StyledGnbWrapper>
   );
 }
--- src/widgets/gnb/ui/styles.ts
+++ src/widgets/gnb/ui/styles.ts
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
-export const StyledGnbWrapper = styled.header`
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-  border-bottom: 1px solid #e0e0e0;
-  box-sizing: border-box;
-  height: 60px;
+export const StyledGnbWrapper = styled.ul`
+  ul {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    border-bottom: 1px solid #e0e0e0;
+    box-sizing: border-box;
+    height: 60px;
+  }
 `;
