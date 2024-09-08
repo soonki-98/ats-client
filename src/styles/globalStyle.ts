12d7a82fe13af3d7f0f4d70831920d0aa9c97016
Min Soonki <minsoonki@ip-192-168-250-26.ap-northeast-2.compute.internal>
2024-09-08 15:39:14 +0900

    .main css 수정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -11,5 +11,10 @@ export const GlobalStyle = createGlobalStyle`
     display: flex;
     height: calc(100vh - 60px);
     width: 100vw;
+    .main {
+      width: 100%;
+      height: 100%;
+      box-sizing: border-box;
+    }
   }
 `;
