32676e656a257f768a53ba940f558bcb1f179ecd
Min Soonki <minsoonki@ip-192-168-250-26.ap-northeast-2.compute.internal>
2024-09-08 15:11:25 +0900

    이력서 리스트 표시

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { _resumeList } from './__mocks/resumes';
+import Resume from './features/resume/ui';
 import { GlobalStyle } from './styles/globalStyle';
 import Gnb from './widgets/gnb/ui';
 import Sidebar from './widgets/sidebar/ui';
@@ -13,16 +15,9 @@ function App() {
           <section className="resume-list">
             <h1>이력서 리스트</h1>
             <ul>
-              {/**
-               * 클릭하면 이력서 상세 페이지로 이동
-               */}
-              <li>
-                <h2>이력서 1</h2>
-                <p>지원자 1 이름</p>
-                <p>지원 직무</p>
-                <p>이력서 업로드 날짜</p>
-                <p>진행 상태 (서류 검토 중, 서류 합격, 불합격)</p>
-              </li>
+              {_resumeList.map((resume) => (
+                <Resume key={resume.id} {...resume} />
+              ))}
             </ul>
           </section>
 
