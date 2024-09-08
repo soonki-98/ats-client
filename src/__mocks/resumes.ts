05eecfc9ec1b4dcbb0530cbe8eb3211f3ccf6ae5
Min Soonki <minsoonki@ip-192-168-250-26.ap-northeast-2.compute.internal>
2024-09-09 00:55:40 +0900

    이력서 목 데이터 수정

--- src/__mocks/resumes.ts
+++ src/__mocks/resumes.ts
@@ -9,6 +9,12 @@ const jobPositions = [
   '프로젝트 매니저',
   '시스템 아키텍트',
   '데이터 분석가',
+  'AI 엔지니어',
+  '기획자',
+  '마케팅 담당자',
+  '영업 담당자',
+  '인사 담당자',
+  '재무 담당자',
 ];
 
 const statuses = ['pending', 'accepted', 'rejected'] as const;
@@ -20,16 +26,21 @@ const generateRandomDate = () =>
 
 const generateResumeList = (count: number): Apis.Resume.Get[] => {
   const names = [
-    '김덕배',
-    '권도안',
-    '강히송',
-    '이승훈',
-    '오린이',
-    '홍길동',
-    '이순신',
-    '유관순',
-    '김유신',
+    '김철수',
+    '이영희',
+    '박민수',
+    '정지영',
+    '오영미',
+    '이지훈',
+    '최지원',
+    '김민지',
+    '박지훈',
+    '이지원',
+    '김민수',
+    '박지영',
+    '이민수',
   ];
+
   const resumeList = [];
 
   for (let i = 1; i <= count; i++) {
@@ -52,92 +63,5 @@ const generateResumeList = (count: number): Apis.Resume.Get[] => {
   return resumeList;
 };
 
-// Initial data
-const initialData: Apis.Resume.Get[] = [
-  {
-    id: '1',
-    applicantName: '김덕배',
-    jobPosition: '프론트엔드 개발자',
-    uploadDate: 1725693536000,
-    status: 'pending',
-    url: './test.pdf',
-  },
-  {
-    id: '2',
-    applicantName: '권도안',
-    jobPosition: '백엔드 개발자',
-    uploadDate: 1722693536000,
-    status: 'accepted',
-    url: './test.pdf',
-  },
-  {
-    id: '3',
-    applicantName: '강히송',
-    jobPosition: 'UI/UX 디자이너',
-    uploadDate: 1724693536000,
-    status: 'rejected',
-    url: './test.pdf',
-  },
-  {
-    id: '4',
-    applicantName: '이승훈',
-    jobPosition: '풀스택 개발자',
-    uploadDate: 1723993536000,
-    status: 'pending',
-    url: './test.pdf',
-  },
-  {
-    id: '5',
-    applicantName: '오린이',
-    jobPosition: '데이터 엔지니어',
-    uploadDate: 1718693536000,
-    status: 'accepted',
-    url: './test.pdf',
-  },
-  {
-    id: '6',
-    applicantName: '홍길동',
-    jobPosition: '프로젝트 매니저',
-    uploadDate: 1717693536000,
-    status: 'pending',
-    url: './test.pdf',
-  },
-  {
-    id: '7',
-    applicantName: '이순신',
-    jobPosition: '시스템 아키텍트',
-    uploadDate: 1716693536000,
-    status: 'accepted',
-    url: './test.pdf',
-  },
-  {
-    id: '8',
-    applicantName: '유관순',
-    jobPosition: '데이터 분석가',
-    uploadDate: 1715693536000,
-    status: 'rejected',
-    url: './test.pdf',
-  },
-  {
-    id: '9',
-    applicantName: '김유신',
-    jobPosition: '프론트엔드 개발자',
-    uploadDate: 1714693536000,
-    status: 'pending',
-    url: './test.pdf',
-  },
-  {
-    id: '10',
-    applicantName: '이순신',
-    jobPosition: '백엔드 개발자',
-    uploadDate: 1713693536000,
-    status: 'accepted',
-    url: './test.pdf',
-  },
-];
-
-// Generate additional 100 items
-const additionalData = generateResumeList(100);
-
 // Combine initial data with additional data
-export const _resumeList = [...initialData, ...additionalData];
+export const _resumeList = generateResumeList(100);
