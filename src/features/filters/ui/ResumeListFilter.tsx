97689147e04907af367cad40bc3ff6fae750bba9
Min Soonki <minsoonki@ip-192-168-250-26.ap-northeast-2.compute.internal>
2024-09-09 00:52:43 +0900

    코드 정리

--- src/features/filters/ui/ResumeListFilter.tsx
+++ src/features/filters/ui/ResumeListFilter.tsx
@@ -12,69 +12,59 @@ const getPositionFromResumeList = (resumeList: typeof _resumeList) => {
 export function ResumeListFilter() {
   const resumeListFilterContext = useResumeListFilter();
 
+  const orderOptions = [
+    { label: '최신 순', value: 'desc' },
+    { label: '오래된 순', value: 'asc' },
+    { label: '이름 순', value: 'name' },
+  ];
+
+  const statusOptions = [
+    { label: '모든 상태', value: null },
+    { label: '합격', value: 'accepted' },
+    { label: '대기중', value: 'pending' },
+    { label: '불합격', value: 'rejected' },
+  ];
+
+  const positionOptions = [
+    {
+      label: '전체 직군',
+      value: null,
+    },
+    ...getPositionFromResumeList(_resumeList).map((position) => ({
+      label: position,
+      value: position,
+    })),
+  ];
+
   const { setOrder, setPosition, setStatus } = resumeListFilterContext;
 
   return (
     <div style={{ display: 'flex', margin: '20px 0', gap: 20 }}>
       <SelectBox
         placeholder="정렬 기준"
-        defaultValue={{ label: '최신 순', value: 'desc' }}
-        options={[
-          { label: '최신 순', value: 'desc' },
-          { label: '오래된 순', value: 'asc' },
-          { label: '이름 순', value: 'name' },
-        ]}
+        defaultValue={orderOptions[0]}
+        options={orderOptions}
         onChange={(newOption) => {
-          const { value } = newOption as {
-            value: 'desc' | 'asc' | 'name';
-            label: string;
-          };
+          const { value } = newOption as { value: 'desc' | 'asc' | 'name' };
           setOrder(value);
         }}
       />
       <SelectBox
         placeholder="직군"
-        defaultValue={{ label: '전체 직군', value: null }}
-        options={[
-          { label: '전체 직군', value: null },
-          ...getPositionFromResumeList(_resumeList).map((position) => ({
-            label: position,
-            value: position,
-          })),
-        ]}
+        defaultValue={positionOptions[0]}
+        options={positionOptions}
         onChange={(newOption) => {
-          const { value } = newOption as {
-            value: string;
-            label: string;
-          };
+          const { value } = newOption as { value: string };
           setPosition(value);
         }}
       />
       <SelectBox
         placeholder="모든 상태"
-        defaultValue={{ label: '모든 상태', value: null }}
-        options={[
-          {
-            label: '모든 상태',
-            value: null,
-          },
-          {
-            label: '합격',
-            value: 'accepted',
-          },
-          {
-            label: '대기중',
-            value: 'pending',
-          },
-          {
-            label: '불합격',
-            value: 'rejected',
-          },
-        ]}
+        defaultValue={statusOptions[0]}
+        options={statusOptions}
         onChange={(newOption) => {
           const { value } = newOption as {
             value: 'accepted' | 'pending' | 'rejected';
-            label: string;
           };
           setStatus(value);
         }}
